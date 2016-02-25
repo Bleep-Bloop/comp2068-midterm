b954de5a0a8e06bf070a31bee3f75ffc4ea1fe93
Ladies <ladies@cs.georgianc.on.ca>
2016-02-25 15:34:49 -0500

    "Final push"

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,21 @@ Sends you to the different pages
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Taisen Colcher ' });
 });
 
 
 
-/* GET people page */
+
 router.get('/people', function(req, res, next) {
   res.render('people', { title: 'People' });
 });
 
 
 
-/* GET contact page */
+
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me ' });
 });
